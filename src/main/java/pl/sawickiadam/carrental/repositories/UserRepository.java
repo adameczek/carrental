942ec59a9aa8d3ad983b2ae001cfd57b2bc8521f
Adam Sawicki <adam2sawicki@gmail.com>
2020-07-02 11:39:41 +0200

    add Repositories

---
+++ src/main/java/pl/sawickiadam/carrental/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package pl.sawickiadam.carrental.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pl.sawickiadam.carrental.models.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
